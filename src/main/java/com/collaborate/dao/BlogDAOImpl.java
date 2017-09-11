57fe2311acee228043683c52e4d65f8f2de14e43
ajay <ajay.algot1993@gmail.com>
2017-09-11 13:57:36 +0530

    changes in blogdaoimpl.

--- src/main/java/com/collaborate/dao/BlogDAOImpl.java
+++ src/main/java/com/collaborate/dao/BlogDAOImpl.java
@@ -2,15 +2,39 @@ package com.collaborate.dao;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.collaborate.model.Blog;
 
-public class BlogDAOImpl implements BlogDAO
-{
-
+@Repository("blogDAO")
+public class BlogDAOImpl implements BlogDAO {
+	
+	@Autowired
+	SessionFactory sessionFactory;
+	
+	public BlogDAOImpl(SessionFactory sessionFactory)
+	{
+		this.sessionFactory=sessionFactory;
+	}
+	@Transactional
 	public boolean createBlog(Blog blog)
 	{
+	try
+	{
+		sessionFactory.getCurrentSession().saveOrUpdate(blog);
+		return true;
+	}
+	catch(Exception e)
+	{
+		System.out.println("Exception Arised"+e);
 		return false;
 	}
+	
+}
 
 	public Blog getBlog(int blogId) {
 		// TODO Auto-generated method stub
