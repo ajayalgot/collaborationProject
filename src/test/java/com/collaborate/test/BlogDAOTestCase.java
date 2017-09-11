70d2bffcf06b5709933edd43d9c45ea8de809a2f
ajay <ajay.algot1993@gmail.com>
2017-09-11 13:59:30 +0530

    blogDAOTestCase is completed

--- src/test/java/com/collaborate/test/BlogDAOTestCase.java
+++ src/test/java/com/collaborate/test/BlogDAOTestCase.java
@@ -3,25 +3,56 @@ package com.collaborate.test;
 import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.collaborate.dao.BlogDAO;
+import com.collaborate.model.Blog;
 
 public class BlogDAOTestCase{
 	static BlogDAO blogDAO;
+	
 	@BeforeClass
 	public static void initialize()
 	{
-		AnnotationConfigApplicationContext annotationConfigApplicationConfig =new AnnotationConfigApplicationContext();
-		
-		annotationConfigApplicationConfig.scan("com.collaborate");
-		annotationConfigApplicationConfig.refresh();
+		AnnotationConfigApplicationContext annotationConfigAppContext=new AnnotationConfigApplicationContext();
+		annotationConfigAppContext.scan("com.collaborate");
+		annotationConfigAppContext.refresh();
+		blogDAO=(BlogDAO)annotationConfigAppContext.getBean("blogDAO");
+	
 	}
+	
 	@Test
-	public void test()
+	public void createBlogtest()
 	{
-		fail("not yet implemented");
+		Blog blog=new Blog();
+		blog.setBlogId(111);
+		blog.setBlogName("Dravid");
+		blog.setBlogContent("Dravid is a Cricket Coach");
+		
+		blog.setCreateDate(new java.util.Date());
+		blog.setStatus("NA");
+		blog.setLikes(0);
+		assertTrue("Problem in Blog Creation",blogDAO.createBlog(blog));
+	
 	}
-
+	
+	@Ignore
+	@Test
+	public void approveBlogTest()
+	{
+		Blog blog=new Blog();
+		blog.setBlogId(1001);
+		blog.setBlogName("Dravid");
+		blog.setBlogContent("Dravid is a Cricket Coach");
+		
+		blog.setCreateDate(new java.util.Date());
+		blog.setStatus("NA");
+		blog.setLikes(0);
+		assertTrue("Problem in approving Blog",blogDAO.approveBlog(blog));	
+	}
+	
+	
+	
 }
