4bef903ca25df398b279433102de184f120eebb5
ajay <ajay.algot1993@gmail.com>
2017-09-11 13:56:35 +0530

    changes in hbconfig

--- src/main/java/com/collaborate/config/HbConfig.java
+++ src/main/java/com/collaborate/config/HbConfig.java
@@ -5,16 +5,20 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.collaborate.dao.BlogDAO;
+import com.collaborate.dao.BlogDAOImpl;
 
 @Configuration
 @ComponentScan("com.collaborate")
 @EnableTransactionManagement
-
 public class HbConfig {
 	
 	// create a datasource object which is used for localSessionFactor
@@ -26,7 +30,7 @@ public class HbConfig {
 		driverManagerDataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
 		driverManagerDataSource.setUsername("hr");
 		driverManagerDataSource.setPassword("hr");
-		
+		System.out.println("Datasource");
 		return driverManagerDataSource;
 	}
 	
@@ -42,15 +46,29 @@ public class HbConfig {
 	}
 	
 	//3.creating session factoryBean using LocalSessionFactory
-	
+	@Bean
 	public SessionFactory getSessionFactory()
 	{
 		LocalSessionFactoryBuilder localSessionFactoryBuilder=new LocalSessionFactoryBuilder(getOracleDataSourceObject());
 		localSessionFactoryBuilder.addProperties(getHibernateProperties());
+		localSessionFactoryBuilder.scanPackages("com.collaborate");
 		System.out.println("SessionFactory Bean Created");
 		
 		return localSessionFactoryBuilder.buildSessionFactory();
 		
 	}
+	
+    @Bean
+    public HibernateTransactionManager getTransaction(SessionFactory sessionFactory)
+    {
+  	 	System.out.println("Transaction");
+		    return new HibernateTransactionManager(sessionFactory);
+    }
+
+   @Bean
+   public BlogDAO getBlogDAO(SessionFactory sessionFactory)
+   {
+  	 return new BlogDAOImpl(sessionFactory);
+   }
 	
 }
